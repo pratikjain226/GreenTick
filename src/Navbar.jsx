0ad19f01fefaa54ea0351cd249efe8c9f0210331
Tejus Tiwari <tejustiwari@gmail.com>
2020-09-04 09:32:23 +0530

    Update Navbar.jsx

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,41 +1,33 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 import "./index.css";
-// import { ReactComponent as Logo } from "./logo.png";
-// import logo from "./logo";
 import logo from "./logo.png";
 
 const Navbar = () => {
   return (
     <>
-      <nav className="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+        <a className="navbar-brand" href="/">
+          <div className="logo">
+            <img src={logo} width="212" height="60" alt="" loading="lazy" />
+          </div>
+        </a>
         <button
           class="navbar-toggler"
           type="button"
           data-toggle="collapse"
           data-target="#navbarResponsive"
         >
-          <span class="navbar-toggler-icon"></span>
+          <span className="navbar-toggler-icon"></span>
         </button>
 
         <div className="collapse navbar-collapse" id="navbarResponsive">
-          {/* <logo
-            src="./logo.png"
-            width="30"
-            height="72"
-            className="d-inline-block align-top"
-            // alt="GREENTICK logo"
-          /> */}
-          <a className="navbar-brand" href="/">
-          <div className="logo"><img src={logo} width="212" height="60" alt="" loading="lazy" /></div>
-          </a>
           <ul className="navbar-nav m-auto">
             <li className="nav-item">
               <div className="home">
-              <NavLink className="nav-link" to="/">
-                Home <span className="sr-only"></span>
-              </NavLink>
-
+                <NavLink className="nav-link" to="/">
+                  Home <span className="sr-only"></span>
+                </NavLink>
               </div>
             </li>
             <li className="nav-item">
@@ -43,15 +35,6 @@ const Navbar = () => {
                 Contest
               </NavLink>
             </li>
-            {/* <li className="nav-item">
-              <NavLink
-
-                className="nav-link"
-                to="/"
-              >
-                Home
-              </NavLink>
-            </li> */}
             <li className="nav-item">
               <NavLink className="nav-link" to="/practice">
                 Practice
@@ -72,16 +55,6 @@ const Navbar = () => {
                 <div class="login"> Login </div>
               </NavLink>
             </li>
-            {/* <li className="nav-item">
-              <NavLink
-                className="nav-link disabled"
-                href="#"
-                tabindex="-1"
-                aria-disabled="true"
-              >
-                Disabled
-              </NavLink>
-            </li> */}
           </ul>
         </div>
       </nav>
